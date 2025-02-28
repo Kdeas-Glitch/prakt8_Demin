9c7e16e7d900500fea592201183c51b54e10586e
Kdeas-Glitch <kde8707@gmail.com>
2025-02-28 11:44:35 +0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    for(i in 1..10)
+    {println("Введите имя поезда Пункт отпр. Пунк назн. Время отправления Время в пути и скорость поезда")
+var num=Poesd(readln().toString(),readln().toString(),readln().toString(),readln().toString(),readln().toDouble(), readln().toDouble())
+    num.Pr()
+    num.St()
+    num.Ps()
+    num.Rast()
+    num.Out()
+    }
+
+}
+
+
